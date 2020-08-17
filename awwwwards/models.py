25c9b1e1c0aac80368953751c0dce731ca5e2311
kangalah <nyongesajoannanjala@gmail.com>
2020-08-17 20:50:12 +0300

    add technologies to the post model

--- awwwwards/models.py
+++ awwwwards/models.py
@@ -27,30 +27,27 @@ class Profile(models.Model):
         instance.profile.save()
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='images/', default='')
-    description = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(auto_now=True)
-    link = models.URLField(max_length=250)
-    country = models.CharField(max_length=50)
-
-    
+    title = models.CharField(max_length=155)
+    url = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
+    technologies = models.CharField(max_length=200, blank=True)
+    photo = ImageField(manual_crop='1280x720')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
-        return self.title
-    
-    class Meta:
-        ordering = ['-date_posted']
-
-    def save_post(self):
-        self.save()
+        return f'{self.title}'
 
     def delete_post(self):
         self.delete()
 
     @classmethod
-    def search(cls,searchterm):
-        search = Post.objects.filter(Q(title__icontains=searchterm)|Q(description__icontains=searchterm)|Q(country__icontains=searchterm))
-        return search
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
 
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
