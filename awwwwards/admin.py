3ebd0d128c00a3aeb3e1eb2695550b8be492c165
kangalah <nyongesajoannanjala@gmail.com>
2020-08-17 21:35:10 +0300

    modify settings to configure

--- awwwwards/admin.py
+++ awwwwards/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Post, rating
+from .models import Profile, Post, Rating
 
 admin.site.register(Profile)
 admin.site.register(Post)
--- awwwwards/forms.py
+++ awwwwards/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post, Profile, Rating
+from .models import Post, Profile
 from pyuploadcare.dj.forms import ImageField
 
 
