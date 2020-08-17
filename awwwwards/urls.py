d967d0cc18c07420815c2fecf3bfe38884afd5c2
kangalah <nyongesajoannanjala@gmail.com>
2020-08-17 20:33:12 +0300

    add the index url

--- awwwwards/urls.py
+++ awwwwards/urls.py
@@ -1,17 +1,21 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.conf.urls import url
+from django.urls import path, include
 from . import views
+from rest_framework import routers
 
-urlpatterns=[
-    url(r'^$', views.index, name='index'),
-    url(r'^update/profile$', views.updateprofile, name='update'),
-    url(r'^profile/$', views.profile, name='profile'),
-    url(r'^new/post$', views.new_post, name='new_post'),
-    url(r'^vote/(?P<post_id>\d+)?$', views.vote, name='vote'), 
-     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^api/post/$', views.PostList.as_view()),
-    url(r'^api/profile/$', views.ProfileList.as_view()),
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('posts', views.PostViewSet)
+router.register('profile', views.ProfileViewSet)
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('api/', include(router.urls)),
+    path('<username>/profile', views.user_profile, name='userprofile'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('profile/<username>/settings', views.edit_profile, name='edit'),
+    path('project/<post>', views.project, name='project'),
+    path('search/', views.search_project, name='search'),
 ]
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
